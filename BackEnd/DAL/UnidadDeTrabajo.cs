d86eff53106868bb4b07617d93d03f99146d795e
Guillermo <65992561+guille7CR@users.noreply.github.com>
2020-06-25 16:46:42 -0600

    Interface y Clase Impl Genericas
    Interface y Clase Impl Genericas
    Unidad de Trabajo

---
+++ BackEnd/DAL/UnidadDeTrabajo.cs
@@ -0,0 +1,46 @@
+﻿using BackEnd.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.DAL
+{
+    class UnidadDeTrabajo<T> : IDisposable where T : class
+    {
+
+        private readonly BDContext context;
+        //public IDALGenerico<Queja> quejaDAL;
+        //public IDALGenerico<TablaGeneral> tablaDAL;
+        public IGenericoDAL<T> genericDAL;
+
+
+        public UnidadDeTrabajo(BDContext _context)
+        {
+            context = _context;
+            genericDAL = new GenericoDALImpl<T>(context);
+            
+        }
+
+        public bool Complete()
+        {
+            try
+            {
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                string msj = e.Message;
+                return false;
+            }
+
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
