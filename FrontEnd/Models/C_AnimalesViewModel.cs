d53791ec1639977c5ec8b7df12d2048c972363e8
Guillermo <65992561+guille7CR@users.noreply.github.com>
2020-07-17 01:13:42 -0600

    Cambios de Estilos y Jquery
    Se incluyen las librerias de unobtrusive validation de jquery y se agregan mejoras en botones, estilos, correciones esteticas

--- FrontEnd/Models/C_AnimalesViewModel.cs
+++ FrontEnd/Models/C_AnimalesViewModel.cs
@@ -14,44 +14,44 @@ namespace FrontEnd.Models
         [Key]
         public int IdAnimal { get; set; }
 
-        [Required(ErrorMessage = "{0} es requerido")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
         [Display(Name = "Nombre")]
-        [MaxLength(50, ErrorMessage = "{0} debe tener una longitud menor o igual a 50")]
+        [MaxLength(50, ErrorMessage = "*{0} debe tener una longitud menor o igual a 50*")]
         [StringLength(50)]
         public string nombre { get; set; }
 
         [StringLength(18)]
-        [Required(ErrorMessage = "{0} es requerido")]
-        [MaxLength(18, ErrorMessage = "{0} debe tener una longitud menor o igual a 18")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [MaxLength(18, ErrorMessage = "*{0} debe tener una longitud menor o igual a 18*")]
         [Display(Name = "Especie")]
         public string especie { get; set; }
 
-        [Required(ErrorMessage = "{0} es requerido")]
-        [MaxLength(50, ErrorMessage = "{0} debe tener una longitud menor o igual a 50")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [MaxLength(50, ErrorMessage = "*{0} debe tener una longitud menor o igual a 50*")]
         [Display(Name = "Raza")]
         public string raza { get; set; }
 
-        [Required(ErrorMessage = "{0} es requerido")]
-        [MaxLength(1, ErrorMessage = "{0} debe tener una longitud igual a 1 caracter")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [MaxLength(1, ErrorMessage = "*{0} debe tener una longitud igual a 1 caracter*")]
         [Display(Name = "Genero")]
         [StringLength(1)]
         public string sexo { get; set; }
 
         [StringLength(100)]
-        [Required(ErrorMessage = "{0} es requerido")]
-        [MaxLength(100, ErrorMessage = "{0} debe tener una longitud menor o igual a 50")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [MaxLength(100, ErrorMessage = "*{0} debe tener una longitud menor o igual a 50*")]
         [Display(Name = "Otras Señas")]
         public string otrasSenas { get; set; }
 
         [StringLength(250)]
-        [Required(ErrorMessage = "{0} es requerido")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
         [Display(Name = "Imagen")]
         public string ImagenURL { get; set; }
 
 
        
         [Display(Name = "Tamaño")]
-        [Required(ErrorMessage = "{0} es requerido")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
         public int IdTamano { get; set; }
         public IEnumerable<C_Tamanos> Tamanos { get; set; }
         public C_Tamanos C_Tamanos { get; set; }
@@ -59,7 +59,7 @@ namespace FrontEnd.Models
 
 
         [Display(Name = "Temperamento")]
-        [Required(ErrorMessage = "{0} es requerido")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
         public int IdTemperamento { get; set; }
         public IEnumerable<C_Temperamentos> Temperamentos { get; set; }
         public C_Temperamentos C_Temperamentos { get; set; }
@@ -67,14 +67,14 @@ namespace FrontEnd.Models
 
 
         [DataType(DataType.Date)]
-        [Required(ErrorMessage = "{0} es requerido")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
         [Display(Name = "Fecha de Nacimiento")]
         public DateTime? fechaNacimiento { get; set; }
 
 
         [StringLength(100)]
-        [Required(ErrorMessage = "{0} es requerido")]
-        [MaxLength(100, ErrorMessage = "{0} debe tener una longitud menor o igual a 50")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [MaxLength(100, ErrorMessage = "*{0} debe tener una longitud menor o igual a 50*")]
         [Display(Name = "Padecimientos")]
         public string padecimientos { get; set; }
 
