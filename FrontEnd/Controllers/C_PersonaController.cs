899f7ed7accf574846bb440424c0e071d535c232
OSCAR BARRANTES <obarrantes40954@ufide.ac.cr>
2020-07-28 01:59:11 -0600

    FlujoPersona
    Queda el flujo Persona Completo

---
+++ FrontEnd/Controllers/C_PersonaController.cs
@@ -0,0 +1,275 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using FrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class C_PersonaController : Controller
+    {
+
+        private C_PersonaViewModel Convertir(C_Persona personas)
+        {
+            C_PersonaViewModel personaViewModel = new C_PersonaViewModel
+            {
+                IdPersona = personas.IdPersona,
+                IdDistrito = (int)personas.IdDistrito,
+                IdProvincia = (int)personas.IdProvincia,
+                IdCanton = (int)personas.IdCanton,
+                nombrePersona = personas.nombrePersona,
+                primerApellido = personas.primerApellido,
+                segundoApellido = personas.segundoApellido,
+                cedula=personas.cedula,
+                correo=personas.correo,
+                sexo = personas.sexo,
+                telefono = personas.telefono,
+                direccion= personas.direccion,
+                estadoCivil= personas.estadoCivil,
+                fechaNacimiento= personas.fechaNacimiento,
+
+
+                
+            };
+            return personaViewModel;
+        }
+
+
+
+        private C_Persona Convertir(C_PersonaViewModel personaViewModel)
+        {
+            C_Persona personas = new C_Persona
+            {
+                IdPersona = personaViewModel.IdPersona,
+                IdDistrito = (int)personaViewModel.IdDistrito,
+                IdProvincia = (int)personaViewModel.IdProvincia,
+                IdCanton = (int)personaViewModel.IdCanton,
+                nombrePersona = personaViewModel.nombrePersona,
+                primerApellido = personaViewModel.primerApellido,
+                segundoApellido = personaViewModel.segundoApellido,
+                cedula = personaViewModel.cedula,
+                correo = personaViewModel.correo,
+                sexo = personaViewModel.sexo,
+                telefono = personaViewModel.telefono,
+                direccion = personaViewModel.direccion,
+                estadoCivil = personaViewModel.estadoCivil,
+                fechaNacimiento = personaViewModel.fechaNacimiento,
+
+            };
+            return personas;
+        }
+
+
+        // GET: C_Personas
+        public ActionResult Index()
+        {
+
+            List<C_Persona> personas;
+
+            using (UnidadDeTrabajo<C_Persona> Unidad = new UnidadDeTrabajo<C_Persona>(new BDContext()))
+            {
+                personas = Unidad.genericDAL.GetAll().ToList();
+            }
+
+            List<C_PersonaViewModel> personasVM = new List<C_PersonaViewModel>();
+
+            C_PersonaViewModel personasViewModel;
+
+            foreach (var item in personas)
+            {
+                personasViewModel = this.Convertir(item);
+
+                using (UnidadDeTrabajo<C_Distrito> unidad = new UnidadDeTrabajo<C_Distrito>(new BDContext()))
+                {
+                    personasViewModel.C_Distrito = unidad.genericDAL.Get(personasViewModel.IdDistrito);
+                }
+
+                using (UnidadDeTrabajo<C_Provincia> unidad = new UnidadDeTrabajo<C_Provincia>(new BDContext()))
+                {
+                    personasViewModel.C_Provincia = unidad.genericDAL.Get(personasViewModel.IdProvincia);
+                }
+
+                using (UnidadDeTrabajo<C_Canton> unidad = new UnidadDeTrabajo<C_Canton>(new BDContext()))
+                {
+                    personasViewModel.C_Canton = unidad.genericDAL.Get(personasViewModel.IdCanton);
+                }
+
+
+                personasVM.Add(personasViewModel);
+
+            }
+            return View(personasVM);
+        }
+
+
+
+        public ActionResult Create()
+        {
+            C_PersonaViewModel personas = new C_PersonaViewModel();
+
+           
+            using (UnidadDeTrabajo<C_Distrito> unidad = new UnidadDeTrabajo<C_Distrito>(new BDContext()))
+            {
+                personas.Distritos = unidad.genericDAL.GetAll().ToList();
+            }
+
+            using (UnidadDeTrabajo<C_Provincia> unidad = new UnidadDeTrabajo<C_Provincia>(new BDContext()))
+            {
+                personas.Provincias = unidad.genericDAL.GetAll().ToList();
+            }
+
+            using (UnidadDeTrabajo<C_Canton> unidad = new UnidadDeTrabajo<C_Canton>(new BDContext()))
+            {
+                personas.Cantons = unidad.genericDAL.GetAll().ToList();
+            }
+
+
+
+            return View(personas);
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(C_Persona personas)
+        {
+            using (UnidadDeTrabajo<C_Persona> unidad = new UnidadDeTrabajo<C_Persona>(new BDContext()))
+            {
+                unidad.genericDAL.Add(personas);
+                unidad.Complete();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            C_PersonaViewModel personas;
+            C_Persona personasDB;
+
+            using (UnidadDeTrabajo<C_Persona> unidad = new UnidadDeTrabajo<C_Persona>(new BDContext()))
+            {
+                personasDB = unidad.genericDAL.Get(id);
+            }
+
+            personas = this.Convertir(personasDB);
+
+            using (UnidadDeTrabajo<C_Distrito> unidad = new UnidadDeTrabajo<C_Distrito>(new BDContext()))
+            {
+                personas.Distritos = unidad.genericDAL.GetAll().ToList();
+            }
+
+            using (UnidadDeTrabajo<C_Provincia> unidad = new UnidadDeTrabajo<C_Provincia>(new BDContext()))
+            {
+                personas.Provincias = unidad.genericDAL.GetAll().ToList();
+            }
+
+            using (UnidadDeTrabajo<C_Canton> unidad = new UnidadDeTrabajo<C_Canton>(new BDContext()))
+            {
+                personas.Cantons = unidad.genericDAL.GetAll().ToList();
+            }
+
+
+            return View(personas);
+
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(C_Persona personas)
+        {
+
+            using (UnidadDeTrabajo<C_Persona> unidad = new UnidadDeTrabajo<C_Persona>(new BDContext()))
+            {
+
+                unidad.genericDAL.Update(personas);
+                unidad.Complete();
+
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            C_Persona personas;
+
+            using (UnidadDeTrabajo<C_Persona> unidad = new UnidadDeTrabajo<C_Persona>(new BDContext()))
+            {
+                personas = unidad.genericDAL.Get(id);
+            }
+
+            C_PersonaViewModel persona = this.Convertir(personas);
+
+            using (UnidadDeTrabajo<C_Distrito> unidad = new UnidadDeTrabajo<C_Distrito>(new BDContext()))
+            {
+                persona.C_Distrito = unidad.genericDAL.Get(persona.IdDistrito);
+            }
+
+            using (UnidadDeTrabajo<C_Provincia> unidad = new UnidadDeTrabajo<C_Provincia>(new BDContext()))
+            {
+                persona.C_Provincia = unidad.genericDAL.Get(persona.IdProvincia);
+            }
+
+            using (UnidadDeTrabajo<C_Canton> unidad = new UnidadDeTrabajo<C_Canton>(new BDContext()))
+            {
+                persona.C_Canton = unidad.genericDAL.Get(persona.IdCanton);
+            }
+
+            return View(persona);
+        }
+
+
+
+        public ActionResult Delete(int id)
+        {
+            C_Persona personas;
+
+            using (UnidadDeTrabajo<C_Persona> unidad = new UnidadDeTrabajo<C_Persona>(new BDContext()))
+            {
+                personas = unidad.genericDAL.Get(id);
+            }
+
+
+            C_PersonaViewModel persona = this.Convertir(personas);
+
+
+            using (UnidadDeTrabajo<C_Distrito> unidad = new UnidadDeTrabajo<C_Distrito>(new BDContext()))
+            {
+                persona.C_Distrito = unidad.genericDAL.Get(persona.IdDistrito);
+            }
+
+            using (UnidadDeTrabajo<C_Provincia> unidad = new UnidadDeTrabajo<C_Provincia>(new BDContext()))
+            {
+                persona.C_Provincia = unidad.genericDAL.Get(persona.IdProvincia);
+            }
+
+            using (UnidadDeTrabajo<C_Canton> unidad = new UnidadDeTrabajo<C_Canton>(new BDContext()))
+            {
+                persona.C_Canton = unidad.genericDAL.Get(persona.IdCanton);
+            }
+
+            return View(persona);
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(C_PersonaViewModel personasView)
+        {
+            using (UnidadDeTrabajo<C_Persona> unidad = new UnidadDeTrabajo<C_Persona>(new BDContext()))
+            {
+                unidad.genericDAL.Remove(this.Convertir(personasView));
+                unidad.Complete();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FrontEnd/Models/C_PersonaViewModel.cs
@@ -0,0 +1,101 @@
+﻿using BackEnd.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FrontEnd.Models
+{
+    public class C_PersonaViewModel
+    {
+        [Key]
+        public int IdPersona { get; set; }
+
+
+        [Display(Name = "Distrito")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        public int IdDistrito { get; set; }
+        public IEnumerable<C_Distrito> Distritos { get; set; }
+        public C_Distrito C_Distrito { get; set; }
+
+
+        [Display(Name = "Provincia")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        public int IdProvincia { get; set; }
+        public IEnumerable<C_Provincia> Provincias { get; set; }
+        public C_Provincia C_Provincia { get; set; }
+
+        [Display(Name = "Cantón")]
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        public int IdCanton { get; set; }
+        public IEnumerable<C_Canton> Cantons { get; set; }
+        public C_Canton C_Canton { get; set; }
+
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "*{0} debe tener una longitud menor o igual a 50*")]
+        [StringLength(50)]
+        public string nombrePersona { get; set; }
+
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Primer Apellido")]
+        [MaxLength(50, ErrorMessage = "*{0} debe tener una longitud menor o igual a 50*")]
+        [StringLength(50)]
+        public string primerApellido { get; set; }
+
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Segundo Apellido")]
+        [MaxLength(50, ErrorMessage = "*{0} debe tener una longitud menor o igual a 50*")]
+        [StringLength(50)]
+        public string segundoApellido { get; set; }
+
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Cédula")]
+        [MaxLength(12, ErrorMessage = "*{0} debe tener una longitud menor o igual a 12*")]
+        [StringLength(12)]
+        public string cedula { get; set; }
+
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Correo Electrónico")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50, ErrorMessage = "*{0} debe tener una longitud menor o igual a 50*")]
+        [StringLength(50)]
+        public string correo { get; set; }
+
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Género")]
+        [MaxLength(12, ErrorMessage = "*{0} debe tener una longitud menor o igual a 12*")]
+        [StringLength(12)]
+        public string sexo { get; set; }
+
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Teléfono")]
+        [MaxLength(12, ErrorMessage = "*{0} debe tener una longitud menor o igual a 12*")]
+        [StringLength(12)]
+        public string telefono { get; set; }
+
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Dirección Exacta")]
+        [MaxLength(250, ErrorMessage = "*{0} debe tener una longitud menor o igual a 250*")]
+        [StringLength(250)]
+        public string direccion { get; set; }
+
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Estado Civíl")]
+        [MaxLength(20, ErrorMessage = "*{0} debe tener una longitud menor o igual a 12*")]
+        [StringLength(20)]
+        public string estadoCivil { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "*{0} es requerido*")]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime? fechaNacimiento { get; set; }
+
+    
+        public  ICollection<C_Adopciones> C_Adopciones { get; set; }
+        public ICollection<C_CasaCuna> C_CasaCuna { get; set; }
+        
+    }
+}
