3571bdcabbd50c01747a40b181a9617a6f4cf62e
OSCAR BARRANTES <obarrantes40954@ufide.ac.cr>
2020-08-03 21:07:03 -0600

    TestBackEnd
    Pruebas de Unidad del BackEnd

---
+++ TestBackEnd/TestCasaCunaDAL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BackEnd.DAL;
+using BackEnd.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBackEnd 
+{
+    [TestClass]
+    public class TestCasaCunaDAL
+    {
+        private UnidadDeTrabajo<C_CasaCuna> unidad;
+
+
+        [TestMethod]
+        public void TestAddGenerico()
+        {
+
+            C_CasaCuna casaCuna = new C_CasaCuna
+            {
+                Metros = "metras"
+            };
+
+
+
+            using (unidad = new UnidadDeTrabajo<C_CasaCuna>(new BDContext()))
+            {
+                unidad.genericDAL.Add(casaCuna);
+                Assert.AreEqual(true, unidad.Complete());
+            }
+
+        }
+
+       
+    }
+}
---
+++ TestBackEnd/TestUserDAL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BackEnd.DAL;
+using BackEnd.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TestBackEnd
+{
+    [TestClass]
+    public class TestUserDAL
+    {
+        private UnidadDeTrabajo<Users> unidad;
+
+
+        [TestMethod]
+        public void TestAdd()
+        {
+
+            Users usuarios = new Users
+            {
+                nombre = "Oscar"
+            };
+
+
+
+            using (unidad = new UnidadDeTrabajo<Users>(new BDContext()))
+            {
+                unidad.genericDAL.Add(usuarios);
+                Assert.AreEqual(true, unidad.Complete());
+            }
+
+        }
+
+
+        
+    }
+}
+    
