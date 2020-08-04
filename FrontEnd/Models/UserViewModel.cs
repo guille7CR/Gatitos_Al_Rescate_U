c988dcc532216dd1e31af5fd993fc4898658dea0
Guillermo <65992561+guille7CR@users.noreply.github.com>
2020-08-04 02:33:59 -0600

    Ajustes de Login y sesion

--- FrontEnd/Models/UserViewModel.cs
+++ FrontEnd/Models/UserViewModel.cs
@@ -10,13 +10,13 @@ namespace FrontEnd.Models
     public class UserViewModel
     {
         public int UserViewModelId { get; set; }
-        [Required(ErrorMessage = "Debe digitar una Nombre de Usuario.")]
+        [Required(ErrorMessage = "Debe digitar una Nombre de Usuario!")]
         [Display(Name = "Usuario")]
         public string UserName { get; set; }
         public string nombre { get; set; }
 
         [DataType(DataType.Password)]
-        [Required(ErrorMessage = "Debe digitar una Contraseña.")]
+        [Required(ErrorMessage = "Debe digitar una Contraseña!")]
         [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
