934f17e37b6c91b1eb17964fa53fdeb987caf86a
Guillermo <65992561+guille7CR@users.noreply.github.com>
2020-08-26 13:05:14 -0600

    Cambios Envia Correo

--- FrontEnd/Controllers/EnviaCorreoController.cs
+++ FrontEnd/Controllers/EnviaCorreoController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,6 +31,8 @@ namespace FrontEnd.Controllers
         {
             try
             {
+                string stImagen;
+
                 MailMessage correo = new MailMessage();
                 correo.From = new MailAddress("gramirez7CR@gmail.com");
                 correo.To.Add("gramirez7CR@gmail.com");
@@ -37,14 +41,26 @@ namespace FrontEnd.Controllers
                 String ruta = Server.MapPath("");
                 fichero.SaveAs(Server.MapPath("/") + "/Cargas/" + fichero.FileName);
                 Attachment adjunto = new Attachment(Server.MapPath("/") + "/Cargas/" + fichero.FileName);
-                correo.Attachments.Add(adjunto);
 
-                correo.Body = "CORREO : " + contactoCorreo+ "<br/>" + "<br/>" +
+
+                correo.Body =
+                              "<div style ='text-align:center;'>" + "<img style ='padding: 0;margin-left:40px; margin-right:40px; display: block' src = 'cid:imagen'>" + "</div>" + "<br/>" + "<br/>" +
+                              "<div style ='text-align:center;font-weight:bold; font-size:18px'>¡GRACIAS POR UTILIZAR GATITOS AL RESCATE!</div>" + "<br/>" + "<br/>" + "<br/>" + "<br/>" +
+                              "CORREO : " + contactoCorreo + "<br/>" + "<br/>" +
                               "NOMBRE : " + contactoNombre + "<br/>" + "<br/>" +
-                              "PARA : " +correo.To+ "<br/>" + "<br/>" +
-                              "MENSAJE : " + mensaje +"<br/>"+ "<br/>" +
-                              "IMAGEN : " +"<br/>" + "<br/>" ;
+                              "PARA : " + correo.To + "<br/>" + "<br/>" +
+                              "MENSAJE : " + mensaje + "<br/>" + "<br/>" +
+                              "ADJUNTO : " + "<br/>" + "<br/>" + "<br/>";
+                               correo.Attachments.Add(adjunto);
 
+
+                AlternateView htmlView = AlternateView.CreateAlternateViewFromString(correo.Body, Encoding.UTF8, MediaTypeNames.Text.Html);
+                stImagen = Server.MapPath("~") + @"/content/assets/img/download.png";
+                LinkedResource img = new LinkedResource(stImagen, MediaTypeNames.Image.Jpeg); img.ContentId = "imagen";
+                htmlView.LinkedResources.Add(img);
+                correo.AlternateViews.Add(htmlView);
+                
+                
                 correo.IsBodyHtml = true;
                 correo.Priority = MailPriority.Normal;
 
